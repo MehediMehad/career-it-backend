5548823a5e0c29fa011a822982b1eac007d86ad6
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2026-09-10 14:22:09 +0600

    feat(auth): update password validation rules to allow shorter and more flexible passwords

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -12,12 +12,8 @@ export enum OtpTypeEnum {
 
 const passwordSchema = z
   .string()
-  .min(8, 'Password must be at least 8 characters long')
-  .max(32, 'Password must be at most 32 characters long')
-  .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-  .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-  .regex(/[0-9]/, 'Password must contain at least one number')
-  .regex(/[@$!%*?&#]/, 'Password must contain at least one special character');
+  .min(6, 'Password must be at least 6 characters long')
+  .max(50, 'Password must be at most 50 characters long');
 
 export const registerSchema = z.object({
   name: z
